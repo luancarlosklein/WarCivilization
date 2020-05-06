93e538720acafd2f14a21c885b4964ceb9b87d06
gabrielbuiar <gabrielbuiar@gmail.com>
2020-05-06 02:44:43 -0400

    Created a map with various hexagons and implemented the map control

--- hexagon.py
+++ hexagon.py
@@ -1,28 +1,59 @@
 import pygame
 
 class hexagon:
-    def __init__(self, border, background = 0, center = [], biome = 0, owner = 0, nTroop = 0, length = 0)
+    def __init__(self, background = 0, center = [0.0], biome = 0, owner = 0, nTroop = 0, length = 0, color = [0,255,0]):
         self.background = background
         self.center = center
         self.biome = biome
         self.owner = owner
         self.nTroop = nTroop
         self.length = length  #30
-        self.color = (0,255,0)
+        self.color = color
+        self.mod = 1.1547     # Escala do hexagono regular mod = 1.1547
+        self.des = [0,0]          # Deslocamento do hexagono
 
-    def show(self, screen):
-        pygame.draw.polygon(screen, self.color, [(self.center[0]-(1.1547*self.length/2),self.center[1]-self.length),(self.center[0]-(1.1547*self.length),
-        self.center[1]),(self.center[0]-(1.1547*self.length/2),self.center[1]+self.length),(self.center[0]+(1.1547*self.length/2),self.center[1]+self.length),
-        (self.center[0]+(1.1547*self.length),self.center[1]),(self.center[0]+(1.1547*self.length/2),self.center[1]-self.length)])
+    def setColor(self, color):
+        self.color = color
+
+    def setDes(self, des):
+        self.des = des
+
+    def getDes(self):
+        return self.des
+
+    def deslocate(self, des):
+        self.des[0] += des[0]
+        self.des[1] += des[1]
+
+    def setLen(self, length):
+        self.length = length
+
+    def getLen(self):
+        return self.length
+
+    def setCenter(self, center):
+        self.center = center
+    
+    def getCenter(self):
+        return self.center
+
+    def show(self, screen, length):
+        pygame.draw.polygon(screen, self.color, [(self.center[0] + self.des[0] - (self.mod*length/2),self.center[1] + self.des[1] -length),(self.center[0] + self.des[0] -(self.mod*length),
+        self.center[1] + self.des[1] ),(self.center[0] + self.des[0] -(self.mod*length/2),self.center[1] + self.des[1] +length),(self.center[0] + self.des[0] +(self.mod*length/2),self.center[1] + self.des[1] +length),
+        (self.center[0] + self.des[0] +(self.mod*length),self.center[1] + self.des[1] ),(self.center[0] + self.des[0] +(self.mod*length/2),self.center[1] + self.des[1] -length)])
+
+        pygame.draw.polygon(screen, [0,0,0], [(self.center[0] + self.des[0] -(self.mod*length/2),self.center[1] + self.des[1] -length),(self.center[0] + self.des[0] -(self.mod*length),
+        self.center[1] + self.des[1] ),(self.center[0] + self.des[0] -(self.mod*length/2),self.center[1] + self.des[1] +length),(self.center[0] + self.des[0] +(self.mod*length/2),self.center[1] + self.des[1] +length),
+        (self.center[0] + self.des[0] +(self.mod*length),self.center[1] + self.des[1] ),(self.center[0] + self.des[0] +(self.mod*length/2),self.center[1] + self.des[1] - length)], 1)    #Desenha a borda do hexagono
 
     def checkClick(self, posMouse):
-        if(abs(posMouse[0] - center[0]) > 1.1547*self.length or abs(posMouse[1] - center[1]) > self.length):
+        if(abs(posMouse[0] - center[0]) > self.mod*self.length or abs(posMouse[1] - center[1]) > self.length):
             return
-        if ((abs(posMouse[0] - center[0]) < 1.1547*self.length/2) and (abs(posMouse[1]-center[1]) < self.length)):
+        if ((abs(posMouse[0] - center[0]) < self.mod*self.length/2) and (abs(posMouse[1]-center[1]) < self.length)):
             print(center)
         elif (posMouse[0] < center[0]):
-            if (((abs(posMouse[1]-(center[1])))/(abs(posMouse[0]-(center[0]-1.1547*self.length)))) < 1.732):
+            if (((abs(posMouse[1]-(center[1])))/(abs(posMouse[0]-(center[0]-self.mod*self.length)))) < 1.732):
                 print(center)
         else:
-            if (((abs(posMouse[1]-(center[1])))/(abs(posMouse[0]-(center[1]+1.1547*self.length)))) < 1.732):
+            if (((abs(posMouse[1]-(center[1])))/(abs(posMouse[0]-(center[1]+self.mod*self.length)))) < 1.732):
                 print(center)
--- mapManager.py
+++ mapManager.py
@@ -1,6 +1,126 @@
 import pygame
+from hexagon import hexagon
 
 class mapManager:
 	def __init__(self, background = 0):
-    	self.hexagons = []
-    	self.background = background
+		self.hexagons = []
+		self.background = background
+		self.nCol = 9			# Numero de hexagonos por coluna
+		self.nRow = 19			# Numero de hexagonos por linha
+		self.hexaLen = 30
+		self.lastPos = [0,0]
+		self.rmClick = False	# Flag para o click do botao direito do mouse
+
+		deslocate = 1
+		pos = [85,50]
+		start = [85,50]
+		color = [0,255,0]
+
+		for i in range (self.nRow):
+			for j in range (self.nCol):
+				self.hexagons.append(hexagon(0, pos, 0, 0, 0, self.hexaLen, color))
+				pos = [pos[0] + 3*self.hexagons[0].mod*self.hexaLen, pos[1]]
+			if not deslocate:
+				self.hexagons.append(hexagon(0, pos, 0, 0, 0, self.hexaLen, color))
+			pos = [start[0], pos[1]+self.hexagons[0].length]
+			if (deslocate):
+				pos[0] -= 1.5*self.hexagons[0].mod*self.hexaLen
+				deslocate = 0
+				color = [50,50,50]
+			else:
+				deslocate = 1
+				color = [50,255,50]
+
+	def set_nCol(self, num):
+		self.nCol = num
+
+	def set_nRow(self,num):
+		self.nRow = num
+	
+	def show(self, screen):
+		for hexagon in self.hexagons:
+			hexagon.show(screen, self.hexaLen)
+		self.check_translation()
+
+	def resizeHexagons(self):
+		i = 0
+
+		pos = self.hexagons[0].center
+		start = self.hexagons[0].center
+		deslocate = 1
+		lenght = self.hexaLen
+		mod = self.hexagons[0].mod
+
+		for row in range (self.nRow):
+			for col in range (self.nCol):
+				self.hexagons[i].setCenter(pos)
+				pos = [pos[0] + 3*mod*lenght, pos[1]]
+				i += 1
+			if not deslocate:
+				self.hexagons[i].setCenter(pos)
+				i += 1
+			pos = [start[0], pos[1]+lenght]
+			if deslocate:
+				pos[0] -= 1.5*mod*lenght
+				deslocate = 0
+			else:
+				deslocate = 1
+
+	def check_translation(self):
+		keys = pygame.key.get_pressed()
+
+		if keys[pygame.K_LEFT]:
+			for hexagon in self.hexagons:
+				hexagon.setDes([hexagon.getDes()[0] + 5, hexagon.getDes()[1]])
+		if keys[pygame.K_RIGHT]:
+			for hexagon in self.hexagons:
+				hexagon.setDes([hexagon.getDes()[0] - 5, hexagon.getDes()[1]])
+		if keys[pygame.K_UP]:
+			for hexagon in self.hexagons:
+				hexagon.setDes([hexagon.getDes()[0], hexagon.getDes()[1] + 5])
+		if keys[pygame.K_DOWN]:
+			for hexagon in self.hexagons:
+				hexagon.setDes([hexagon.getDes()[0], hexagon.getDes()[1] - 5])
+		if keys[pygame.K_SPACE]:
+			mousePos = pygame.mouse.get_pos()
+			if self.rmClick:
+				for hexagon in self.hexagons:
+					hexagon.deslocate([mousePos[0] - self.lastPos[0], mousePos[1] - self.lastPos[1]])
+			else:
+				self.rmClick = True
+			self.lastPos = mousePos
+		else:
+			if self.rmClick == True:
+				self.rmClick = False
+
+		for event in pygame.event.get():
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				if event.button == 4:
+					#for hexagon in self.hexagons:
+					#	hexagon.setLen(hexagon.getLen()+5)
+					self.hexagons[0].setLen(self.hexagons[0].getLen()+5)
+					self.hexaLen += 5
+					self.resizeHexagons()
+
+				if event.button == 5:
+					# for hexagon in self.hexagons:
+					# 	hexagon.setLen(hexagon.getLen()-5)
+					self.hexagons[0].setLen(self.hexagons[0].getLen()-5)
+					self.hexaLen -= 5
+					self.resizeHexagons()
+				
+			# 	if event.button == 3:
+			# 		print("DOWN")
+			# 		if (self.rmClick):
+			# 			for hexagon in self.hexagons:
+			# 				hexagon.des(pygame.mouse.get_pos() - self.lastPos)
+			# 		else:
+			# 			self.rmClick = True
+			# 		self.lastPos = pygame.mouse.get_pos()
+
+			# if event.type == pygame.MOUSEBUTTONUP:
+			# 	if event.button == 3:
+			# 		print("UP")
+			# 		self.rmClick = False
+
+
